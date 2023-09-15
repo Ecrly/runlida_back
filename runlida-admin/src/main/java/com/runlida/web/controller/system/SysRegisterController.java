0b79dea0f30a65ba5ae7a99d40386c4da8c7757e
SasaKiHaise <2846698558@qq.com>
2023-09-15 15:34:17 +0800

    init

---
+++ runlida-admin/src/main/java/com/runlida/web/controller/system/SysRegisterController.java
@@ -0,0 +1,38 @@
+package com.runlida.web.controller.system;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.runlida.common.core.controller.BaseController;
+import com.runlida.common.core.domain.AjaxResult;
+import com.runlida.common.core.domain.model.RegisterBody;
+import com.runlida.common.utils.StringUtils;
+import com.runlida.framework.web.service.SysRegisterService;
+import com.runlida.system.service.ISysConfigService;
+
+/**
+ * 注册验证
+ * 
+ * @author ruoyi
+ */
+@RestController
+public class SysRegisterController extends BaseController
+{
+    @Autowired
+    private SysRegisterService registerService;
+
+    @Autowired
+    private ISysConfigService configService;
+
+    @PostMapping("/register")
+    public AjaxResult register(@RequestBody RegisterBody user)
+    {
+        if (!("true".equals(configService.selectConfigByKey("sys.account.registerUser"))))
+        {
+            return error("当前系统没有开启注册功能！");
+        }
+        String msg = registerService.register(user);
+        return StringUtils.isEmpty(msg) ? success() : error(msg);
+    }
+}
---
+++ runlida-common/src/main/java/com/runlida/common/core/domain/model/RegisterBody.java
@@ -0,0 +1,11 @@
+package com.runlida.common.core.domain.model;
+
+/**
+ * 用户注册对象
+ * 
+ * @author ruoyi
+ */
+public class RegisterBody extends LoginBody
+{
+
+}
